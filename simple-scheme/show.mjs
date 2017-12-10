c8c3ecca1ad59684010f93417dd51a9ae6d21780
Richard Cobbe <rcobbe@rcobbe.net>
2017-12-10 15:49:05 -0800

    Document show function

--- simple-scheme/show.mjs
+++ simple-scheme/show.mjs
@@ -1,5 +1,7 @@
 "use strict";
 
+// Convert an object to a human-readable representation.  Uses the argument's
+// show method, if it exists; otherwise falls back on toString.
 export function show(x) {
     if (typeof(x.show) == 'function') {
         return x.show();
