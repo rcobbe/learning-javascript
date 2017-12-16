ba0b32059941ff0c622f70f7afcd38ea14f3d9fd
Richard Cobbe <rcobbe@rcobbe.net>
2017-12-15 17:47:41 -0800

    Rewrite Map API in OO style; add 'show' support

--- simple-scheme/map.mjs
+++ simple-scheme/map.mjs
@@ -1,43 +1,70 @@
 "use strict";
 
 import * as list from "./list.mjs";
+import * as show from "./show.mjs";
 
 // Defines a functional map from keys to values.  Uses === to compare keys.
 // For now, implement as alist for simplicity; can replace this with a
 // more efficient representation as need requires.
 
 var _tag = Symbol("tag");
-var _data = Symbol("data");
 var _key = Symbol("key");
 var _val = Symbol("val");
-var mapTag = Symbol("mapTag");
+var _next = Symbol("next");
+var _showElements = Symbol("showElements");
 
 export function isMap(x) {
-    return x.hasOwnProperty(_tag) && x[_tag] === mapTag;
+    return x.hasOwnProperty(_tag);
 }
 
 export var empty = {
-    [_tag]: mapTag,
-    [_data]: list.empty
+    [_tag]: _tag,  // value isn't important; we only check for existence
+                   // of the field
+    extend: function extend(k, v) { return extendImpl(k, v, this); },
+    lookup: function lookup(k) { return undefined; },
+    show: function show() { return "Map()"; },
+    [_showElements]: function showElements() { return ''; }
 };
 
-// extend :: a b Map<a, b> -> Map<a, b>
-export function extend(k, v, m) {
-    console.assert(isMap(m));
-    return { [_tag]: mapTag,
-             [_data]: list.cons({ [_key]: k, [_val]: v }, m[_data])
-           };
+// function interface to extension, for better chaining:
+//   extend('a', 1, extend('b', 2, extend('c', 3, m)))
+// reads better than
+//   m.extend('c', 3).extend('b', 2).extend('a', 1)
+// in some circumstances.
+export function extend(k, v, map) {
+    console.assert(isMap(map));
+    return map.extend(k, v);
 };
 
-// lookup :: k Map<k, v> -> U<v, undefined>
-// returns undefined object if key not found
-export function lookup(k, m) {
-    console.assert(isMap(m));
-    for (var x = m[_data]; !list.isEmpty(x); x = list.cdr(x)) {
-        let head = list.car(x);
-        if (head[_data] === k) {
-            return head[_val];
+function extendImpl(k, v, map) {
+    console.assert(isMap(map));
+    return {
+        [_tag]: _tag,
+        [_key]: k,
+        [_val]: v,
+        [_next]: map,
+        extend: function extend(k, v) { return extendImpl(k, v, this); },
+        lookup: function lookup(k) {
+            if (this[_key] === k) {
+                return this[_val];
+            } else {
+                return this[_next].lookup(k);
+            }
+        },
+        show: function show() {
+            return 'Map(' + this[_showElements](true) + ')';
+        },
+        // Returns string representation of this node's key-value pair
+        // with a leading ', ', unless we're the first in the map
+        [_showElements]: function(isFirst) {
+            let rest = this[_next][_showElements](false);
+            rest =
+                '(' + show.show(this[_key]) + ', ' +
+                show.show(this[_val]) + ')' + rest;
+            if (!isFirst) {
+                rest = ', ' + rest;
+            }
+            return rest;
         }
-    }
-    return undefined;
+    };
 }
