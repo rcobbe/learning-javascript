e8f902e866ece95b66364eefe5e92b487d6e5a71
Richard Cobbe <rcobbe@rcobbe.net>
2017-12-15 17:48:06 -0800

    Sketch out value representation

--- simple-scheme/config.mjs
+++ simple-scheme/config.mjs
@@ -1,5 +1,18 @@
 "use strict";
 
+// Value ::=
+//   number
+//   {value_tag: 'str-lit', val: string}
+//   {value_tag: 'bool-lit', val: boolean}
+//   {value_tag: 'symbol', val: string}
+//   {value_tag: 'empty-list'}
+//   {value_tag: 'void'}
+//   {value_tag: 'pair', car: Addr, cdr: Addr}
+//   {value_tag: 'closure', rho: Env, formals: [str, ...], body: Expr}
+//   {value_tag: 'continuation', k: Continuation}
+//   {value_tag: 'undefined'}
+//   {value_tag: 'primitive', f: [Value, ...] Store -> [Value, Store]}
+
 // Expr ::=
 //   number
 //   {op: 'str-lit', val: string}
